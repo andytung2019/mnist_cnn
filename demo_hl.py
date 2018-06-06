5a79e960ccc6dac68a2222df48e0c09aea5b56a3
andytunt2019 <andytung@gmail.com>
2018-06-07 01:31:25 +0800

    gpu number

--- demo_hl.py
+++ demo_hl.py
@@ -39,7 +39,7 @@ class InferenceConfig(coco.CocoConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
     GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
+    IMAGES_PER_GPU = 4
 
 class MyCheck():
 
